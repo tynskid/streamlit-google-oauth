9b8c12a85493f51a927de1d5571586718dcfc2e1
tynskid <67418614+tynskid@users.noreply.github.com>
2023-04-05 22:47:47 -0400

    Update __init__.py

--- streamlit_google_oauth/__init__.py
+++ streamlit_google_oauth/__init__.py
@@ -29,9 +29,7 @@ async def revoke_token(client, token):
 
 
 def login_button(authorization_url, app_name, app_desc):
-    st.markdown('''<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">''',
-    unsafe_allow_html=True)
+
 
     container = f'''
     <div class="container-fluid border py-4 px-4 border-primary">
